079625f9f9e3bcf16132866ceeee639ad39cc19c
patrickrwilson <36892066+patrickrwilson@users.noreply.github.com>
2018-06-04 09:23:55 -0500

    Initial Upload
    Initial Upload

---
+++ pavlov__bwchange/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Pavlov Bandwidth Change",
+
+    'summary': """
+        Bandwidth Changes for Pavlov Media""",
+
+    'description': """
+        Bandwidth Changes module for Pavlov Media
+    """,
+
+    'author': "Pavlov Media",
+    'website': "http://www.pavlovmedia.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/pavlov_bwchange.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ pavlov__bwchange/models/bw_change.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+#Main Bandwidth Change Records Model
+class BandwidthChange(models.Model):
+     _name = 'pavlov_bwchange.change'
+
+#General
+     name = fields.Char(string="Title", required=True)
+     account = fields.Many2one('res.partner', string="Account")
+     eta = fields.Date(string="ETA", required=False)
+     description = fields.Text(string="Description", required=False)
+
+#Property Circuit Details (Based on Deployment)
+     actual_transport = fields.Float(string="Actual Transport Speed (Mbps)", store=True)
+     circuit_paid_by = fields.Many2one ('pavlov_bwchange.ownerselection',string="Circuit Paid By")
+     sold_transport = fields.Float(string="Sold Transport Speed/DIA Download (OffNet Mbps)", store=True)
+     dia_upload = fields.Float(string="DIA Upload (OffNet Mbps)", store=True)
+     tesseractive_service = fields.Boolean(string="Tesseractive Service?")
+     tesseractiv_speed = fields.Float(string="Tesseractiv Speed (OnNet Mbps) *If Applicable", store=True)
+     websnap = fields.Boolean(string="Websnap?")
+     circuit_landdate = fields.Date(string="Circuit Land Date")
+     circuit_id = fields.Char(string="Circuit ID")
+
+#Current MDF Equipment (Filled out by NOC)
+     router = fields.Many2one('pavlov_bwchange.devicespeedselection', string="Router")
+     mikrotik = fields.Many2one('pavlov_bwchange.devicespeedselection', string="MikroTik")
+     distro_switch = fields.Many2one('pavlov_bwchange.devicespeedselection', string="Distrution Switch (Between Buildings)")
+     core_switch = fields.Many2one('pavlov_bwchange.devicespeedselection', string="Internal Core Switch (Site Backbone)")
+     user_switch = fields.Many2one('pavlov_bwchange.devicespeedselection', string="Site User Switch Port Speed")
+     trunked = fields.Boolean(string="Trunked?")
+     verified_by = fields.Char(string="Verified By")
+     verified_date = fields.Date(string ="Verified Date")
+
+#User/Unit Experience (Contracted Info)
+     websnap_download = fields.Float(string="Websnap Download Speed (Mbps)", store=True)
+     package_download = fields.Float(string="User/Unit Package Download Speed (Mbps)", store=True)
+     package_upload = fields.Float(string="User/Unit Package Upload Speed (Mbps)", store=True)
+     unregistered_tesseractiv = fields.Float(string="Unregistered Tesserativ (Mbps)", store=True)
+     unregistered_websnap = fields.Float(string="Unregistered WebSnap (Mbps)", store=True)
+     unregistered_download = fields.Float(string="Unregistered Download (Mbps)", store=True)
+     unregistered_upload = fields.Float(string="Unregistered Upload (Mbps)", store=True)
+     reason_for_change = fields.Char(string="Reason for Package Change")
+
+#Contract Information
+     contract_signed_date = fields.Date(string="Contract Signed Date")
+
+#Device Speed Selections Model
+class BandwidthChangeDeviceSpeedSelection(models.Model):
+     _name = 'pavlov_bwchange.devicespeedselection'
+
+#General
+     name = fields.Char(string="Title", required=True)
+
+#Device Speed Selections Model
+class BandwidthChangeOwnerSelection(models.Model):
+     _name = 'pavlov_bwchange.ownerselection'
+
+#General
+     name = fields.Char(string="Title", required=True)
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
