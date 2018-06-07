7096a9b13b64e22ebf1dfd17b471b46901fd1f2c
patrickrwilson <36892066+patrickrwilson@users.noreply.github.com>
2018-06-07 15:32:51 -0500

    fixed icon and security

--- pavlov__bwchange/__manifest__.py
+++ pavlov__bwchange/__manifest__.py
@@ -15,7 +15,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Bandwidth',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -26,6 +26,8 @@
         # 'security/ir.model.access.csv',
         'views/pavlov_bwchange.xml',
         'views/res_partner.xml',
+        'security/security.xml',
+        'security/ir.model.access.csv',
     ],
     # only loaded in demonstration mode
     'demo': [
