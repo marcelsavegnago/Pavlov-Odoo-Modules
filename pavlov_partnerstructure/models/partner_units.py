3b247a81e040f22e41035509aaf263541262f4b3
patrickrwilson <36892066+patrickrwilson@users.noreply.github.com>
2018-06-11 16:39:32 -0500

    Added some OCA modules

--- pavlov_partnerstructure/models/partner_units.py
+++ pavlov_partnerstructure/models/partner_units.py
@@ -9,3 +9,4 @@ class partner_units(models.Model):
      name = fields.Char(string="Title", required=True)
      account = fields.Many2one('res.partner', string="Account")
      floor = fields.Many2one('pavlov_partnerstructure.floors', string="Floor")
+     building = fields.Many2one('pavlov_partnerstructure.buildings', related='floor.building', string="Building", readonly=True)
