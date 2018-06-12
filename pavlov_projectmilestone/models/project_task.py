dbb2ca96cce309c3fcf83669ccc96222ee420aad
patrickrwilson <36892066+patrickrwilson@users.noreply.github.com>
2018-06-12 15:06:13 -0500

    milestone kanban view changes

--- pavlov_projectmilestone/models/project_task.py
+++ pavlov_projectmilestone/models/project_task.py
@@ -3,4 +3,9 @@ from odoo import models, fields, api
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
-    milestone = fields.Many2one('pavlov_projectmilestone.milestones', string="Milestones")
+    @api.model
+    def _read_group_milestone_ids(self,milestone,domain,order):
+        milestone_ids = self.env['pavlov_projectmilestone.milestones'].search([('projects', '=', self.env.context['default_project_id'])])
+        return milestone_ids
+
+    milestone = fields.Many2one('pavlov_projectmilestone.milestones', string="Milestones", group_expand='_read_group_milestone_ids')
