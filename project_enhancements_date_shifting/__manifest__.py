550895c218af9c18276b32211638b875f365687b
patrickrwilson <36892066+patrickrwilson@users.noreply.github.com>
2019-02-12 17:39:21 -0600

    [FIX] Settings

--- project_enhancements_date_shifting/__manifest__.py
+++ project_enhancements_date_shifting/__manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Project',
-    'version': '12.0.1.0.0',
+    'version': '1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base',
