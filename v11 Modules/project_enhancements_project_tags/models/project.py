b9fdd5167dcc241cc3eaedcd9d0ea04a51d5b6b2
patrickrwilson <36892066+patrickrwilson@users.noreply.github.com>
2019-02-11 18:05:21 -0600

    [v11]

--- v11 Modules/project_enhancements_project_tags/models/project.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-
-class Project(models.Model):
-    _inherit = 'project.project'
-
-    tag_ids = fields.Many2many('project.tags',
-                                 string="Tags",
-                                 help="The Tags that this Project is related to.")
