451d3924d6aa6d41abdde1a972c612864c8bbdd8
patrickrwilson <36892066+patrickrwilson@users.noreply.github.com>
2019-03-04 11:00:16 -0600

    [Add] Project Task Timer

---
+++ project_enhancements_timer/models/project_task.py
@@ -0,0 +1,29 @@
+from odoo import api, fields, models
+
+class ProjectTask(models.Model):
+    _inherit = "project.task"
+
+    timer_started = fields.Boolean(default=False)
+    start_timer_date = fields.Datetime()
+    end_timer_date = fields.Datetime()
+
+    @api.multi
+    def start_timer(self):
+        self.write({'timer_started': True,
+                    'start_timer_date': fields.Datetime.now()})
+
+    @api.multi
+    def end_timer(self):
+        self.write({'end_timer_date': fields.Datetime.now()})
+        context = dict(self.env.context)
+        context.update({'start_timer_date': self.start_timer_date, 'end_timer_date': self.end_timer_date,'default_project_id': self.project_id.id, 'default_task_id': self.id})
+        view_id = self.env.ref('project_enhancements_timer.timesheet_entry_view_form')
+        return {
+            'view_id': view_id.ids,
+            'view_type': 'form',
+            "view_mode": 'form',
+            'res_model': 'timesheet_entry',
+            'type': 'ir.actions.act_window',
+            'context': context,
+            'target': 'new'
+        }
--- project_enhancements_timer/wizards/timesheet_entry.py
+++ project_enhancements_timer/wizards/timesheet_entry.py
@@ -7,27 +7,26 @@ from odoo.exceptions import Warning
 class TimesheetEntry(models.TransientModel):
     _name = "timesheet_entry"
 
-    start_date = fields.Datetime(string="Start Date", readonly=True)
-    end_date = fields.Datetime(string="End Date", readonly=True)
-    description = fields.Text(string="Description", required=True, default="Time Worked on Ticket")
+    start_timer_date = fields.Datetime(string="Start Date", readonly=True)
+    end_timer_date = fields.Datetime(string="End Date", readonly=True)
+    description = fields.Text(string="Description", required=True, default="Time Worked on Task")
     duration = fields.Float('Duration', readonly=True)
     project_id = fields.Many2one('project.project', string="Project", readonly=True)
     task_id = fields.Many2one('project.task', string="Task", readonly=True)
-    ticket_id = fields.Many2one('helpdesk.ticket',string="Ticket", readonly=True)
 
     @api.model
     def default_get(self, default_fields):
         context = self._context
-        s_date = context.get('start_date')
-        e_date = context.get('end_date')
+        s_date = context.get('start_timer_date')
+        e_date = context.get('end_timer_date')
         diff = datetime.datetime.strptime(
             e_date, "%Y-%m-%d %H:%M:%S") - datetime.datetime.strptime(s_date, "%Y-%m-%d %H:%M:%S")
         duration = float(diff.days) * 24 + (float(diff.seconds) / 3600)
         final_output = round(duration, 2)
         res = super(TimesheetEntry, self).default_get(default_fields)
         res.update({
-            'start_date': s_date,
-            'end_date': e_date,
+            'start_timer_date': s_date,
+            'end_timer_date': e_date,
             'duration': final_output,
         })
         return res
@@ -44,11 +43,10 @@ class TimesheetEntry(models.TransientModel):
                     'task_id': self.task_id.id,
                     'project_id': self.project_id.id,
                     'unit_amount': self.duration,
-                    'account_id': self.project_id.analytic_account_id.id,
-                    'helpdesk_ticket_id': self.ticket_id.id
+                    'account_id': self.project_id.analytic_account_id.id
                     }
             analytic_line_rec = self.env['account.analytic.line'].create(vals)
-            self.ticket_id.write({'timesheet_ids': [(4, 0, [analytic_line_rec.id])],
+            self.task_id.write({'timesheet_ids': [(4, 0, [analytic_line_rec.id])],
                             'timer_started': False})
         else:
             raise Warning(_("Please link Project to this Task to save the entry"))
@@ -56,4 +54,4 @@ class TimesheetEntry(models.TransientModel):
     @api.multi
     def discard_entry(self):
         self.description = "Test"
-        self.ticket_id.write({'timer_started':False, 'start_date': False, 'end_date':False})
+        self.task_id.write({'timer_started':False, 'start_timer_date': False, 'end_timer_date':False})
