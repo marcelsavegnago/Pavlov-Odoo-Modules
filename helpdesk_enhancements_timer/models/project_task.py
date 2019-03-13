adbddf1bf9b674b4d484573ec7582eb32c596d34
patrickrwilson <36892066+patrickrwilson@users.noreply.github.com>
2019-03-13 11:49:27 -0500

    [FIX] Timer Logic

--- helpdesk_enhancements_timer/models/project_task.py
+++ helpdesk_enhancements_timer/models/project_task.py
@@ -31,22 +31,32 @@ class ProjectTask(models.Model):
                                     'user_id': self._uid})]})
     @api.multi
     def task_end_timer(self):
-        active_timer = self.env['timer.timer'].search([('user_id','=',self._uid)])
-        active_timer.write({'end_timer_date': fields.Datetime.now()})
-        context = dict(self.env.context)
-        context.update({'default_timer_id': active_timer.id,
-                        'start_timer_date': active_timer.start_timer_date,
-                        'end_timer_date': active_timer.end_timer_date,
-                        'default_project_id': active_timer.project_id.id,
-                        'default_task_id': active_timer.task_id.id,
-                        'default_ticket_id': active_timer.ticket_id.id})
-        view_id = self.env.ref('project_enhancements_timer.task_timesheet_entry_view_form')
-        return {
-            'view_id': view_id.ids,
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': 'task_timesheet_entry',
-            'type': 'ir.actions.act_window',
-            'context': context,
-            'target': 'new'
-        }
+        active_timer = self.env['timer.timer'].search([('task_id','=',self.id),('user_id','=',self._uid)])
+        if not active_timer:
+            stop_view_id = self.env.ref('project_enhancements_timer.timer_stop_warning_view_form')
+            return {'view_id': stop_view_id.ids,
+                    'view_type': 'form',
+                    'view_mode': 'form',
+                    'res_model': 'timer_warning',
+                    'type': 'ir.actions.act_window',
+                    'target': 'new'
+                    }
+        else:
+            active_timer.write({'end_timer_date': fields.Datetime.now()})
+            context = dict(self.env.context)
+            context.update({'default_timer_id': active_timer.id,
+                            'start_timer_date': active_timer.start_timer_date,
+                            'end_timer_date': active_timer.end_timer_date,
+                            'default_project_id': active_timer.project_id.id,
+                            'default_task_id': active_timer.task_id.id,
+                            'default_ticket_id': active_timer.ticket_id.id})
+            view_id = self.env.ref('project_enhancements_timer.task_timesheet_entry_view_form')
+            return {
+                    'view_id': view_id.ids,
+                    'view_type': 'form',
+                    'view_mode': 'form',
+                    'res_model': 'task_timesheet_entry',
+                    'type': 'ir.actions.act_window',
+                    'context': context,
+                    'target': 'new'
+                    }
