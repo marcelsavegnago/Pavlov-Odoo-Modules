44bebdbc2ecde8764d564bd1822d449a0dfd9390
patrickrwilson <36892066+patrickrwilson@users.noreply.github.com>
2019-03-15 13:45:44 -0500

    [FIX] Timers

--- project_enhancements_timer/models/timer.py
+++ project_enhancements_timer/models/timer.py
@@ -4,6 +4,7 @@ class Timer(models.Model):
     _name = "timer.timer"
 
     name = fields.Char(string="Name")
+    active = fields.Boolean(string="Active", default=True)
     start_timer_date = fields.Datetime()
     end_timer_date = fields.Datetime()
     user_id = fields.Many2one('res.users', string="User")
