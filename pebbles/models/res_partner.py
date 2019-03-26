f1d9507d9a643ff9f68436e7f0c08bf2ccd988b0
patrickrwilson <36892066+patrickrwilson@users.noreply.github.com>
2019-03-26 09:48:57 -0500

    [UPDATE] Documents

--- pebbles/models/res_partner.py
+++ pebbles/models/res_partner.py
@@ -3,24 +3,5 @@ from odoo import models, fields, api
 class Partner(models.Model):
     _inherit = 'res.partner'
 
-    pebbles_key_id = fields.Char(string="Pebbles Key ID")
-
-    @api.multi
-    def open_myaccount_url(self,pebbles_key_id):
-        return {
-        'name'     : 'MyAccount',
-        'res_model': 'ir.actions.act_url',
-        'type'     : 'ir.actions.act_url',
-        'target'   : 'new',
-        'url'      : ("https://myaccount.pavlovmedia.net/")
-        }
-
-    @api.multi
-    def open_createaccount_url(self):
-        return {
-        'name'     : 'MyAccount',
-        'res_model': 'ir.actions.act_url',
-        'type'     : 'ir.actions.act_url',
-        'target'   : 'new',
-        'url'      : "https://myaccount.pavlovmedia.net/"
-        }
+    external_systems_flag = fields.Boolean(string="External System?")
+    external_systems_id = fields.Char(string="External System ID")
